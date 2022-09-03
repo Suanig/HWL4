b6416b5de001fe25f328a5054c7613a7d82673e3
Suanig <suanig88@gmail.com>
2022-09-03 17:10:59 +0300

    Create HWL4T2

---
+++ HWL4T2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int modul = Math.Abs(number);
+int sum ()
+{
+var digits = modul.ToString().Select(t=>int.Parse(t.ToString())).ToArray();
+int sum = 0;
+for (int i = 0; i < digits.Length; ++i)
+sum=sum + digits[i];
+return sum;
+}
+
+Console.WriteLine($"Результат сложения всех цифр числа {number} = {sum()}");
