b6e99b5c6ddd074337195d7e929e522fb0a04be7
Suanig <suanig88@gmail.com>
2022-09-03 16:52:46 +0300

    Create HWL4T1

---
+++ HWL4T1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число A ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень B числа A ");
+int degreeB = Convert.ToInt32(Console.ReadLine());
+int numberB = 1;
+
+int DBA(int numberB)
+{
+   int result = 1;
+    while (numberA < Math.Pow(numberA, degreeB))
+    result = numberB * numberA;
+    return result;
+}
+
+
+Console.WriteLine($"Результат возведения числа А в степень В = {DBA(numberB)}");
