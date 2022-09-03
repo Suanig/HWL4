ba76552fe2c3b77c52c04f5353b997e1093d2b64
Suanig <suanig88@gmail.com>
2022-09-03 17:23:33 +0300

    Create HWL4T3

---
+++ HWL4T3/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < 8; i++)
+array[i] = rand.Next(101); 
+var str = string.Join(" ", array);
+Console.WriteLine($"Сгенерированный массив из 8 чисел - {str}");
